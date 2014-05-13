7d531d62e2251b47aa8eff53337b99a8684effcf
alejandrodob <alejandrodob@gmail.com>
2014-05-13 17:02:49 +0200

    Added missing ';'

--- templates/admin/js/featured_apps.js
+++ templates/admin/js/featured_apps.js
@@ -4,10 +4,10 @@
   $.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken)
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
     }
-  })
+  });
   
   $SCRIPT_ROOT = {{ request.script_root|tojson|safe }};
   function add(app_id) {
