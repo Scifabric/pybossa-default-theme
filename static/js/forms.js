20fc849cfe468713fbb229de3762e42203d884a6
Daniel Lombraña González <teleyinex@gmail.com>
2014-05-22 11:23:51 +0200

    Use new jquery plugin for text

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,33 +1,22 @@
 (function(){
     var resource = location.pathname.split('/')[1];
-    var name = getNameInputFor(resource);
-    var slug = getSlugInputFor(resource);
 
-    name.addEventListener('keyup', function(){
-        slug.value = makeSlugFromName(name.value);
-    });
-
-    function makeSlugFromName(name){
-        var not_valid_chars = /([$#&\/|]+)/g;
-        return name.toLowerCase().trim().replace(not_valid_chars, "").replace(/( )+/g, "-");
+    function makeSlug(text) {
+        var not_valid_chars = /([$#%·~!¡?"¿'=)(!&\/|]+)/g;
+        return text.toLowerCase().trim().replace(not_valid_chars, "").replace(/( )+/g, "");
     }
 
-    function getNameInputFor(resource){
-        if (resource === 'app'){
-            return document.getElementById('name');
-        }
-        if (resource === 'account'){
-            return document.getElementById('fullname');
-        }
-    }
+    if ( resource === 'app' ) {
+        $("#name").bind('textchange', function (event, previousText) {
+          $('#short_name').val(makeSlug($(this).val()));
+        });
 
-    function getSlugInputFor(resource){
-        if (resource === 'app'){
-            return document.getElementById('short_name');
-        }
-        if (resource === 'account'){
-            return document.getElementById('name');
-        }
+    }
+    
+    if ( resource === 'account' ) {
+        $("#fullname").bind('textchange', function (event, previousText) {
+          $('#name').val(makeSlug($(this).val()));
+        });
     }
 
 }());
