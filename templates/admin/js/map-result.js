bcf69e578e3dacb5a7a6a2f5b020439f6ee97a30
egzontina <egzontina.krasniqi@gmail.com>
2016-08-24 15:16:04 +0200

    Minor chnages on map files.

---
+++ templates/admin/js/map-result.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var map = L.map('map').setView([42.662115143057235, 21.16320312023163], 2);
+  map.scrollWheelZoom.disable();
+  L.tileLayer('http://{s}.tiles.mapbox.com/v3/georges.jjc3k7b1/{z}/{x}/{y}.png', {
+      attribution: "{{ _('Data Source') }}:" + "<a href='http://opendatakosovo.org'>Open Data Kosovo</a>",
+      maxZoom: 10
+  }).addTo(map);
+});å
