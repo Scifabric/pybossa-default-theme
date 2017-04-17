f13db79caa4cb36e4646026db72730395f0178f9
dtymo <dt.tymo@gmail.com>
2017-04-17 13:35:14 -0400

    renamed all 'projecttypes' tags to 'categories'

--- templates/projects/js/categories.js
+++ templates/projects/js/categories.js
@@ -1,12 +1,12 @@
 <script>
   $("#show-types").on('click', function(){
-    $("#project-types").show();
+    $("#categories").show();
     $("#show-types").addClass("hidden");
     $('#hide-types').show();
   })
 
   $("#hide-types").on('click', function(){
-    $("#project-types").hide();
+    $("#categories").hide();
     $("#hide-types").hide();
     $("#show-types").removeClass("hidden");
   })
