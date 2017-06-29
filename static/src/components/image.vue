51ae2b8e1217f263a01ca032497e8d1b4e4e4319
Daniel Lombraña González <teleyinex@gmail.com>
2017-06-29 12:10:41 +0200

    Publish Unpublish.

--- static/src/components/image.vue
+++ static/src/components/image.vue
@@ -18,10 +18,17 @@
           <input class="form-control" v-model="data.title" placeholder="Write a nice title" type="text">
       </div>
       <markdown-editor v-model="data.body"></markdown-editor/>
-      <button class="btn btn-primary">Update blogpost</button>
+      <div class="action-btns">
+        <button class="btn btn-warning" v-on:click="update">Save draft</button>
+        <div v-if="canPublish">
+          <button v-if="this.data.published" class="btn btn-primary" v-on:click="publish(false)">Unpublish</button>
+          <button v-else class="btn btn-primary" v-on:click="publish(true)">Publish</button>
+        </div>
+      </div>
     </div>
 </template>
 <script>
+import axios from 'axios'
 import VueCoreImageUpload from 'vue-core-image-upload'
 import { markdownEditor } from 'vue-simplemde'
 
@@ -33,14 +40,37 @@ export default {
   data() {
     return {
       src: 'http://via.placeholder.com/500x375',
+      blogpost_id: null,
       data: {project_id: 580,
              title: '',
-             body: 'body'}
+             body: '',
+             published: false
+             }
     }
   },
   methods: {
     imageuploaded(res) {
-      this.src = res.media_url;
+      this.src = res.media_url
+      this.data.title = res.title
+      this.data.body = res.body
+      this.blogpost_id = res.id
+    },
+    update(){
+        axios.put(this.puturl, this.data).then(function(response){console.log(response)})
+    },
+    publish(flag){
+        this.data.published = flag
+        axios.put(this.puturl, this.data).then(function(response){console.log(response)})
+    }
+  },
+  computed: {
+    puturl(){
+        if (this.blogpost_id) return '/api/blogpost/' + this.blogpost_id
+        else return '/api/blogpost'
+    },
+    canPublish(){
+        if (this.data.title === '' || this.data.body === '') return false
+        else return true
     }
   }
 }
@@ -55,5 +85,10 @@ export default {
 
 .g-core-image-upload-btn {
     position: absolute !important;
+}
+
+.action-btns {
+    display: flex;
+    justify-content: space-between;
 }
 </style>
