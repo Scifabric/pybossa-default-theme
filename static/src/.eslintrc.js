1e2ef2e8e76836a52f8e6df3cbfacb03b4fd6f3d
ywrsusan <wyuan35@bloomberg.net>
2019-05-03 15:06:25 -0400

    lintor config

--- static/src/.eslintrc.js
+++ static/src/.eslintrc.js
@@ -34,6 +34,10 @@ module.exports = {
     'generator-star-spacing': 0,
 
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    'template-curly-spacing': 'off',
+
+    indent: 'off'
   }
 }
