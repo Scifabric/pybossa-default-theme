92522f42a3c52bd20ce682ef195d8801dbad94f9
mroseber <mroseberry@bloomberg.net>
2019-06-21 17:03:17 -0400

    Fix lint errors.

--- static/src/test/utils/getters.js
+++ static/src/test/utils/getters.js
@@ -1,14 +1,14 @@
-export function bindGetters(unboundGetters, state) {
+export function bindGetters (unboundGetters, state) {
   const gettersDescriptor = Object.entries(unboundGetters).reduce(getterReducer, {});
   const getters = Object.defineProperties({}, gettersDescriptor);
   return getters;
 
-  function getterReducer(objectDescriptor, [getterName, getterFunc]) {
+  function getterReducer (objectDescriptor, [getterName, getterFunc]) {
     return Object.assign(
       objectDescriptor,
       {
         [getterName]: {
-          get() {
+          get () {
               return getterFunc(state, getters);
           }
         }
@@ -16,4 +16,3 @@ export function bindGetters(unboundGetters, state) {
     );
   }
 }
-
