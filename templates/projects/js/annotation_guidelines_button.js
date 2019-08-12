399402eff695ed53b8c95faa4185a92d6c486e87
Maria Rivera <mrivera144@bloomberg.net>
2019-08-12 10:58:59 -0400

    Fix review comments

--- templates/projects/js/annotation_guidelines_button.js
+++ templates/projects/js/annotation_guidelines_button.js
@@ -1,10 +1,8 @@
-
 <script>
 $(".collapse-guidelines-button").on('click', function(){
-    var showInstructionsMsg = 'Show Instructions';
-    var hideInstructionsMsg = 'Hide Instructions';
     $(this).text(function(i, v){
-        return v === showInstructionsMsg ? hideInstructionsMsg : showInstructionsMsg
+        var text = v.startsWith('Show') ? 'Hide' : 'Show';
+        return text + ' Instructions';
     });
 });
 </script>
