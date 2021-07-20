9f216e607ea1c2a95d96bceee097a2815a0bd618
Kory Becker <50708624+kbecker42@users.noreply.github.com>
2021-07-20 13:29:26 -0400

    RDISCROWD-4451 Collapsible left-nav and responsive browse tasks table (#308)
    * Added left-nav icons. Added left-nav collapsible control.
    
    * Expand main content.
    
    * Refactor.
    
    * Icons active color white.
    
    * Responsive sizing of browse tasks table.
    
    * Style.
    
    * Alignment of chevron in left-nav.
    
    * Persist left nav state. Refactored sass.
    
    * Set tooltip when collapsed.
    
    * Changed from localStorage to cookie for smooth rendering collapse state upon page load from server template.
    
    * Smooth effect for left-nav on page load.
    
    * Cleanup.
    
    * Moved to separate js file.

---
+++ static/js/nav.js
@@ -0,0 +1,63 @@
+$(function() {
+  const leftNavCollapseKey = 'leftNavCollapse';
+  const menu = $('#menu');
+  const btnCollapse = menu.find('a.btn-expand-collapse');
+
+  function setCookie(name, value, days=365) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  const leftNavTooltips = () => {
+    // Set the tooltip to show the menu item text.
+    menu.find('a.list-group-item').each((index, e) => {
+      $(e).attr('title', menu.hasClass('collapsed') ? $(e).text() : '');
+    });
+  };
+
+  const onCollapseLeftNav = () => {
+    // Toggle collapsed state of left-nav menu.
+    menu.toggleClass('collapsed');
+    // Hide right chevrons of left-nav menu items.
+    menu.find('a.list-group-item i.pull-right').toggleClass('fa fa-chevron-right');
+    // Toggle the chevron in the collapse button right/left or left/right.
+    btnCollapse.find('i').toggleClass('fa-chevron-left fa-chevron-right');
+    // Set tooltips on left-nav menu.
+    leftNavTooltips();
+    // Expand width of the page when left-nav is collapsed.
+    $('.container').toggleClass('w-100');
+    // Save settings, use a cookie for smooth rendering of page upon load from server.
+    const isCollapse = getCookie('leftNavCollapse') || false;
+    setCookie('leftNavCollapse', !isCollapse);
+  };
+
+  const initializeLeftNav = () => {
+    // Initialize left-nav menu state.
+    const isCollapse = getCookie('leftNavCollapse');
+    isCollapse && leftNavTooltips();
+
+    // Setup event handler on left-nav collapse button click.
+    btnCollapse.click(onCollapseLeftNav);
+
+    // Enable animation effort on left-nav collapse.
+    $('.container').addClass('animated');
+  };
+
+  initializeLeftNav();
+});
