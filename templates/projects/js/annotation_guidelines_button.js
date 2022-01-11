88fd8227102f688592dc306202144f721c2c4eb4
Kory Becker <50708624+kbecker42@users.noreply.github.com>
2022-01-11 11:16:20 -0500

    Disable submit buttons and show message upon task timeout. (#336)

--- templates/projects/js/annotation_guidelines_button.js
+++ templates/projects/js/annotation_guidelines_button.js
@@ -4,4 +4,20 @@ $(".collapse-guidelines-button").on('click', function(){
         return (v.startsWith('Show') ? 'Hide' : 'Show') + ' Instructions';
     });
 });
+
+function setup_task_timeout_display(timeout) {
+    timeout && parseInt(timeout) &&
+        setTimeout(function() {
+            var selector = 'button.submit-button,button.submit-last-button';
+            var msg = 'This task has timed-out after ' + timeout + ' seconds.';
+
+            window.pybossaNotify(msg, true, 'error');
+
+            document.querySelectorAll(selector).forEach(function(button) {
+                button.disabled = true;
+                button.setAttribute('title', msg);
+            });
+        },  timeout * 1000);
+}
+setup_task_timeout_display({{ project.info.timeout }});
 </script>
