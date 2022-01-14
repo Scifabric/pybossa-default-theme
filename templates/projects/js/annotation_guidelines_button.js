a2f0f220bb83efde92e90e4e31b2cf888f8ef7d7
Kory Becker <50708624+kbecker42@users.noreply.github.com>
2022-01-14 14:22:07 -0500

    RDISCROWD-4787 Fix timeout. (#338)
    * Disable submit buttons and show message upon task timeout.
    
    * Fix timeout.
    
    * Updated message to account for partial seconds remaining.

--- templates/projects/js/annotation_guidelines_button.js
+++ templates/projects/js/annotation_guidelines_button.js
@@ -5,11 +5,11 @@ $(".collapse-guidelines-button").on('click', function(){
     });
 });
 
-function setup_task_timeout_display(timeout) {
+function setup_task_timeout_display(timeout, original_timeout) {
     timeout && parseInt(timeout) &&
         setTimeout(function() {
             var selector = 'button.submit-button,button.submit-last-button';
-            var msg = 'This task has timed-out after ' + timeout + ' seconds.';
+            var msg = 'This task has timed-out after ' + (original_timeout || timeout) + ' seconds.';
 
             window.pybossaNotify(msg, true, 'error');
 
@@ -19,5 +19,5 @@ function setup_task_timeout_display(timeout) {
             });
         },  timeout * 1000);
 }
-setup_task_timeout_display({{ project.info.timeout }});
+setup_task_timeout_display({{ project.timeout or 0 }}, {{ project.original_timeout or project.timeout or 0 }});
 </script>
