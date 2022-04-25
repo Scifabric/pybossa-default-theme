f2c8ba4688c037087f0312e94713bb81b22d3b41
Kory Becker <50708624+kbecker42@users.noreply.github.com>
2022-04-25 11:21:28 -0400

    RDISCROWD-5136 (#352)
    * Disable timeout banner flag in js.
    
    * Added taskTimeOutInterval to disable banner message per project via clearInterval(taskTimeOutInterval).
    
    * Fix.

--- templates/projects/js/annotation_guidelines_button.js
+++ templates/projects/js/annotation_guidelines_button.js
@@ -7,7 +7,7 @@ $(".collapse-guidelines-button").on('click', function(){
 
 var taskTimeOutInterval;
 function setup_task_timeout_display(timeout, original_timeout) {
-    timeout && parseInt(timeout) &&
+    if (timeout && parseInt(timeout)) {
         taskTimeOutInterval = setTimeout(function() {
             var selector = 'button.submit-button,button.submit-last-button';
             var msg = 'This task has timed-out after ' + (original_timeout || timeout) + ' seconds.';
@@ -19,6 +19,7 @@ function setup_task_timeout_display(timeout, original_timeout) {
                 button.setAttribute('title', msg);
             });
         },  timeout * 1000);
+    }
 }
 setup_task_timeout_display({{ project.timeout or 0 }}, {{ project.original_timeout or project.timeout or 0 }});
 </script>
