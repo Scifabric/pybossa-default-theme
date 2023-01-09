991a60f3d930d81c427b7d7c6dfd79231bc9b1e6
nsyed22 <nsyed22@bloomberg.net>
2023-01-09 16:52:01 -0500

    image upload callback working

--- static/js/image_upload.js
+++ static/js/image_upload.js
@@ -1,17 +1,7 @@
 // onImageUpload callback
 
 const TaskPresenterImageUploader = {
-    initialize: guidelines => {
-
-        console.log("INITIALIZING IMAGE UPLOADER");
-
-        guidelines.on('summernote.image.upload', (e, f) => {
-            console.log("INSERTING IMAGE 1")
-            editor.summernote('insertNode', imgNode);
-        });
-
-        guidelines.on("summernote.change", e => {
-            console.log("CHANGED!!!")
-        });
+    uploadImages: (files, editor) => {
+        console.log("uploadImages: uploading images", files)
     }
 }
