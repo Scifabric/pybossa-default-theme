aa4fa5465d999c94e2c88e162095a2b1c4a541ab
nsyed22 <nsyed22@bloomberg.net>
2023-01-11 13:31:24 -0500

    insert image node

--- static/js/image_upload.js
+++ static/js/image_upload.js
@@ -2,18 +2,23 @@
 
 const TaskPresenterImageUploader = {
 
-    uploadImages: (files, short_name) => {
-        let data = {"files": files}
-        console.log("uploadImages request", data)
+    uploadImages: (files, short_name, summernote) => {
 
-        res = $.ajax({
+        //TODO: check image sizes
+        var fd = new FormData();
+
+        Array.from(files).forEach(file => { fd.append("image", file) });
+        $.ajax({
             type: 'POST',
             url: '/project/' + short_name + "/tasks/taskpresenterimageupload",
-            contentType: "application/json",
-            dataType: 'json',
-            data: JSON.stringify(data)
-        });
-        console.log("uploadImages response", res)
-        return res
+            data: fd,
+            processData: false,
+            contentType: false,
+        }).done(response => {
+            let image = $('<img>').attr('src', response.imgurls[0]);
+            summernote.summernote("insertNode", image[0]);
+        }).fail(response => {
+            console.log("upload image failed. response:", response)
+        })
     }
 }
