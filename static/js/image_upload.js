abffdef3e13d02080a3afa76d15c216830b0d926
nsyed22 <nsyed22@bloomberg.net>
2023-01-11 10:29:42 -0500

    adds image upload request

--- static/js/image_upload.js
+++ static/js/image_upload.js
@@ -1,7 +1,21 @@
 // onImageUpload callback
 
 const TaskPresenterImageUploader = {
+
     uploadImages: (files, editor) => {
-        console.log("uploadImages: uploading images", files)
+        let URL = '/project/'
+        let short_name = "testproject"
+        let data = {"files": files, "test": "hello"}
+        console.log("uploadImages request", data)
+
+        res = $.ajax({
+            type: 'POST',
+            url: URL + short_name + "/tasks/taskpresenterimageupload",
+            contentType: "application/json",
+            dataType: 'json',
+            data: JSON.stringify(data)
+        });
+        console.log("uploadImages response", res)
+        return res
     }
 }
