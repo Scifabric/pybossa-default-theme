e87703ad620fc6ef109e9a19a095fedf1e9679d5
nsyed22 <nsyed22@bloomberg.net>
2023-01-12 10:23:22 -0500

    file size check

--- static/js/image_upload.js
+++ static/js/image_upload.js
@@ -1,13 +1,18 @@
 // onImageUpload callback
 
 const TaskPresenterImageUploader = {
-
+    MAX_FILE_SIZE_MB: 5,
     uploadImages: (files, short_name, summernote) => {
+        let fd = new FormData();
+        Array.from(files).forEach(file => {
+            if (file.size / 1024 / 1024 < TaskPresenterImageUploader.MAX_FILE_SIZE_MB) {
+                fd.append("image", file);
+            }
+            else {
+                alert("Images must be less than " + TaskPresenterImageUploader.MAX_FILE_SIZE_MB + " mb.")
+            }
+        });
 
-        //TODO: check image sizes
-        var fd = new FormData();
-
-        Array.from(files).forEach(file => { fd.append("image", file) });
         $.ajax({
             type: 'POST',
             url: '/project/' + short_name + "/tasks/taskpresenterimageupload",
@@ -15,8 +20,12 @@ const TaskPresenterImageUploader = {
             processData: false,
             contentType: false,
         }).done(response => {
-            let image = $('<img>').attr('src', response.imgurls[0]);
-            summernote.summernote("insertNode", image[0]);
+            response.imgurls.forEach(imgurl => {
+                let image = $('<img>').attr('src', imgurl).on('load', () => {
+                    image.css('width', Math.min(summernote.width(), this.width))
+                });
+                summernote.summernote("insertNode", image[0]);
+            })
         }).fail(response => {
             console.log("upload image failed. response:", response)
         })
