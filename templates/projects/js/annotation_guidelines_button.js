639dfb86d10d9424bf037c676f47276db9a01446
Noor Syed <noorsyed1a@gmail.com>
2023-01-31 13:41:43 -0500

    RDISCROWD-5608 Keep guidelines hidden (#390)
    * store guidelines state
    
    * make state project specific
    
    ---------
    
    Co-authored-by: nsyed22 <nsyed22@bloomberg.net>

--- templates/projects/js/annotation_guidelines_button.js
+++ templates/projects/js/annotation_guidelines_button.js
@@ -1,7 +1,20 @@
 <script>
-$(".collapse-guidelines-button").on('click', function(){
-    $(this).text(function(i, v){
-        return (v.startsWith('Show') ? 'Hide' : 'Show') + ' Instructions';
-    });
+const guidelines = $("#collapse-guidelines")
+const button = $(".collapse-guidelines-button")
+const key = '{{project.short_name}}' + "-guidelines-state"
+
+if (localStorage.getItem(key) === "closed") {
+  guidelines.collapse("hide")
+  button.text('Show Instructions')
+}
+
+button.on('click', function(){
+        if (localStorage.getItem(key) === "closed") {
+            localStorage.setItem(key, "open")
+            button.text('Hide Instructions')
+        } else {
+            localStorage.setItem(key, "closed")
+            button.text('Show Instructions')
+        }
 });
 </script>
